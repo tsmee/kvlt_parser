4e2516d03cf80c23cd472ebc21b227efc04e427c
tsmee <draftpodium@gmail.com>
2020-07-30 15:02:13 +0300

    fixed some async nonsense

--- async_parser.py
+++ async_parser.py
@@ -1,8 +1,9 @@
-from bs4 import BeautifulSoup
-import csv
 import asyncio
+import csv
+
 import aiohttp
 from aiofiles.threadpool import open as aioopen
+from bs4 import BeautifulSoup
 
 with open('saved_bands.txt', 'rb') as f_bands:
     saved_bands = f_bands.read().splitlines()
@@ -14,13 +15,8 @@ with open('out4.csv', 'r', encoding='utf-8') as read_obj:
 
 
 async def fetch(session, url):
-    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36'}
+    headers ={'authority': 'www.metal-archives.com', 'upgrade-insecure-requests': '1', 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36', 'cookie': '__cfduid=d329291db14d28ff998419f0b55ee220e1595992006; PHPSESSID=4rn2vpibgi7ngft226q2pqv7s3; __utmc=235797405; __utmz=235797405.1595992007.1.1.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); phpbb3_qh6oy_u=1; phpbb3_qh6oy_k=; phpbb3_qh6oy_sid=a24b775c7dfa9410fbcc7ff74039885a; __utma=235797405.1086835883.1595992007.1596064171.1596106774.9; __cf_bm=5fa20e3ce539687f4ac2402dc7380bcdbac2138d-1596109392-1800-AZQQP4b+DX84Oi5gddoKqMPOn8vJaUs+nRVyH0sX1FqzRq0pPCv4sEbYucN588SYfHFE08de97itXs697KP757E=; __utmt=1; __utmb=235797405.6.10.1596106774'}
     async with session.get(url, headers=headers) as resp:
-        print(resp.content_length)
-        print(resp.headers)
-        await resp.content.read()
-        if resp.content_type == 'text/html; charset=UTF-8':
-            return None
         return await resp.content.read()
 
 
@@ -31,7 +27,6 @@ async def bound_fetch(sem, url, session):
 
 
 async def save_logo(session, sem, band_name, path, img_url):
-
     logo = await fetch(session, img_url)
     if logo:
         async with aioopen(path, 'wb') as f_logo:
@@ -76,7 +71,5 @@ async def mass_action(r):
             await responses
 
 
-
-
 loop = asyncio.get_event_loop()
-loop.run_until_complete(asyncio.ensure_future(mass_action(3)))
+loop.run_until_complete(asyncio.ensure_future(mass_action(1)))
